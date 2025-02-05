9b08a479692feba2502df716dc5176e134fac6d7
Peter Ballard <pjb28@students.uwf.edu>
2025-02-05 11:55:37 -0600

    Did some planning

---
+++ htppClient.hpp
@@ -0,0 +1,19 @@
+// request user to enter the name of the http server followed by
+// the name of a file on that server
+
+// client should send a vaild HTTP/1.1 request ro the server for the file
+
+// client program should display all the responses received from the server
+    // Make sure the client program guides the user about the right usage
+    // before promting the user and waiting on a client request
+
+    // also make sure the user can request for more files in the same way 
+    // rather than exiting immediately
+
+    // If server is still active and listening, it should respond accordingly
+
+    // If server has stopped working, it should also be indicated in the running
+    // client program
+
+    // Be verbose !!!
+    
